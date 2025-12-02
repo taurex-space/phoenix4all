14aed9f89683602a018def3c10850ca48cf9781f
Ahmed F. Al-Refaie <ahmed.f.alrefaie@gmail.com>
2025-12-02 08:02:41 +0000

    Added SVO in sources init

--- src/phoenix4all/sources/__init__.py
+++ src/phoenix4all/sources/__init__.py
@@ -1,16 +1,16 @@
 from .core import InterpolationMode, PhoenixDataFile, PhoenixSource
 from .hiresfits import HiResFitsSource
 from .registry import find_source, list_sources, register_source
-from .synphot import SynphotSource
 from .svo import SVOSource
+from .synphot import SynphotSource
 
 __all__ = [
     "HiResFitsSource",
     "InterpolationMode",
     "PhoenixDataFile",
     "PhoenixSource",
-    "SynphotSource",
     "SVOSource",
+    "SynphotSource",
     "find_source",
     "list_sources",
     "register_source",
