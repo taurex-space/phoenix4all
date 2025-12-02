0ec74dee78c972156df06a968984125dc6952fb4
Ahmed F. Al-Refaie <ahmed.f.alrefaie@gmail.com>
2025-12-02 11:11:40 +0000

    Initial pollux

---
+++ src/phoenix4all/sources/pollux.py
@@ -0,0 +1,33 @@
+from urllib.parse import urljoin
+import requests
+import pathlib
+from typing import Optional
+
+POLLUX_BASE_URL = "https://pollux.oreme.org/"
+
+AVAILABLE_MODELS = [
+    "AMBRE",
+    "BT-Dusty",
+    "CMFGEN",
+    "CMFGEN-OB-LMC-24",
+    "CMFGEN-OB-SMC-24",
+    "CMFGEN-VMS-Z0p01ZSun",
+    "CMFGEN-VMS-Z0p1ZSun",
+    "CMFGEN-VMS-ZSMC",
+    "POPSYCLE",
+    "RSG",
+    "STAGGER",
+    "STAGGER-INTENSITY",
+    "STAGGER-RVS"
+]
+
+
+
+
+def load_spectrum(
+        spectrum_pk: str,
+        model_name: str,
+        base_path: Optional[pathlib.Path] = None,
+        base_url: Optional[str] = None,
+):
+    pass
